f3c566ae15915fe19acfdebc4c31c4739ef5fe90
Mauritshidde <43985321+Mauritshidde@users.noreply.github.com>
2024-07-05 14:29:24 +0200

    werkt

--- Game/obj/src/chestopen.cpp.d
+++ Game/obj/src/chestopen.cpp.d
@@ -1,22 +1,19 @@
-obj/src/chestopen.cpp.bc: src/chestopen.cpp src/chestopen.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/defines.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/ti/getcsc.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/stdint.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/cdefs.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/stdbool.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/graphx.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/stdlib.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/time.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/keypadc.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/math.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/debug.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/stdio.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/stdarg.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/ti/screen.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/stddef.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/ti/ui.h src/gfx/gfx.h \
+obj/src/chestopen.cpp.bc: src/chestopen.cpp src/chestopen.h src/defines.h \
+  /home/maurits/CEdev/include/ti/getcsc.h \
+  /home/maurits/CEdev/include/stdint.h \
+  /home/maurits/CEdev/include/cdefs.h \
+  /home/maurits/CEdev/include/stdbool.h \
+  /home/maurits/CEdev/include/graphx.h \
+  /home/maurits/CEdev/include/stdlib.h \
+  /home/maurits/CEdev/include/time.h \
+  /home/maurits/CEdev/include/keypadc.h \
+  /home/maurits/CEdev/include/math.h /home/maurits/CEdev/include/debug.h \
+  /home/maurits/CEdev/include/stdio.h \
+  /home/maurits/CEdev/include/stdarg.h \
+  /home/maurits/CEdev/include/ti/screen.h \
+  /home/maurits/CEdev/include/stddef.h \
+  /home/maurits/CEdev/include/ti/ui.h src/gfx/gfx.h \
   src/gfx/global_palette.h src/gfx/Dungeonset.h src/gfx/PlayerSprite.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/compression.h \
-  src/player.h src/sprite.h src/animated.h \
-  /home/kaio/Documents/Coderclass/CEdev/include/vector/vector.h \
-  src/tilemap.h src/iswalkable.h src/weapon.h
+  /home/maurits/CEdev/include/compression.h src/player.h src/sprite.h \
+  src/animated.h src/vector/vector.h src/tilemap.h src/iswalkable.h \
+  src/weapon.h
